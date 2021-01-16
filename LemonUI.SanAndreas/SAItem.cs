724a936677907df86c298ae88ea625fdca7b29ac
Hannele Ruiz <justlemoncl@gmail.com>
2021-01-16 17:36:39 -0300

    Added basics of the item

--- LemonUI.SanAndreas/SAItem.cs
+++ LemonUI.SanAndreas/SAItem.cs
@@ -1,9 +1,54 @@
-using LemonUI;
+using LemonUI.Elements;
+using System.Drawing;
 
 namespace LemonUI.SanAndreas
 {
+    /// <summary>
+    /// Simple item used on the San Andreas menus.
+    /// </summary>
     public class SAItem
     {
+        #region Fields
 
+        internal readonly ScaledText title = new ScaledText(PointF.Empty, "");
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The title of the item.
+        /// </summary>
+        public string Title
+        {
+            get => title.Text;
+            set => title.Text = value;
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new item with a title.
+        /// </summary>
+        public SAItem(string title)
+        {
+            this.title.Text = title;
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Recalculates the position of the menu with the last known values.
+        /// </summary>
+        private void Recalculate()
+        {
+
+        }
+
+        #endregion
     }
 }
